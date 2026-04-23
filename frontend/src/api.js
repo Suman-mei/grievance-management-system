bbaa6e025f64f35fffba135d586896336310db61
Suman mei <suman.2428cseaiml20@kiet.edu>
2026-04-23 23:05:36 +0530

    Fix frontend for Render deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://grievance-management-system-gbvk.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://grievance-management-system-gbvk.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
